de2515cbf44618edee50d7647c498f6005bde2e9
bileyraker <69011843+bileyraker@users.noreply.github.com>
2024-12-14 15:53:21 -0600

    Bind snake to on_snake_death signal connection

--- grid.gd
+++ grid.gd
@@ -9,7 +9,7 @@ const CELL_SIZE : int = 32
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	game_tick_timer.timeout.connect(snake.data.on_time_tick)
-	snake.data.died.connect(on_snake_death)
+	snake.data.died.connect(on_snake_death.bind(snake))
 
-func on_snake_death() -> void:
+func on_snake_death(_snake : SnakeData) -> void:
 	game_tick_timer.stop()
